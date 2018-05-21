2403dcd197c470175ae7bec69c26b386aa78d367
Juanjo Alegría <jotaj.8a@gmail.com>
2018-05-21 03:06:10 -0400

    Añadido módulo con utilidades de imágenes

---
+++ src/utils/image.py
@@ -0,0 +1,80 @@
+"""Módulo con funciones utilitarias para trabajar con imágenes
+"""
+
+import os
+import cv2
+
+
+def extract_patches(image_path, patch_height, patch_width,
+                    stride_rows, stride_columns):
+    """Extrae parches desde una imagen, con determinado tamaño y cada cierta
+    cantidad de pixeles.
+
+    Args:
+        - image_path: str. Ruta a la imagen de la cual se quieren extraer los
+        parches.
+        - patch_height: int. Altura en pixeles requerida de los parches.
+        - patch_width: int. Ancho en pixeles  requerido de los parches.
+        - stride_rows: int. Intervalo, en pixeles, según el cual avanzar en la
+        dirección de las filas.
+        - stride_columns: int. Intervalo, en pixeles, según el cual avanzar en
+        la dirección de las columnas.
+
+    Returns:
+        - dict[str: np.array], donde cada llave es un string que indica la fila
+        y la columna de donde fue extraido el parche (en formato
+        "i(\\d*)_j(\\d*)"), y cada valor asociado es l parche correspondiente.
+
+    """
+    img = cv2.imread(image_path)
+    roi_height, roi_width, _ = img.shape
+    name_template = "i{row}_j{column}"
+    patches_dict = {}
+    for row in range(0, roi_height, stride_rows):
+        for column in range(0, roi_width, stride_columns):
+            patch = img[row: row + patch_height,
+                        column: column + patch_width]
+            if patch.shape[:2] == (patch_height, patch_width):
+                patch_name = name_template.format(row=row, column=column)
+                patches_dict[patch_name] = patch
+
+    return patches_dict
+
+
+def save_patches(patches_dict, prefix_name, dst_dir):
+    """Guarda un diccionario con imágenes a dst_dir.
+
+    Todas las imágenes serán guardadas en dst_dir, con el nombre
+    {prefix_name}_{image_key}.jpg, donde image_key es la llave asociada en el
+    diccionario a cada imagen.
+
+    Args:
+        - patches_dict: dict[str: np.array], diccionario con imágenes como
+        valores y con los sufijos para el nombre de cada imagen como llaves.
+        - prefix_name: str. Nombre común que llevarán todas las imágenes.
+        - dst_dir: str. Directorio donde se guardarán las imágenes.
+    """
+
+    template = prefix_name + "_{suffix}.jpg"
+    for suffix_key in patches_dict:
+        image_name = template.format(suffix=suffix_key)
+        image_path = os.path.join(dst_dir, image_name)
+        image = patches_dict[suffix_key]
+        cv2.imwrite(image_path, image)
+
+
+def tif_to_jpeg(tif_image_path, dst_dir, jpeg_quality=100):
+    """Convierte una imagen en formato tif a formato jpeg.
+
+    Args:
+        - tif_image_path: str. Ubicación de la imagen en formato tif.
+        - dst_dir: Directorio donde se quiere guardar la imagen en formato
+        jpeg.
+        - jpeg_quality. int. Calidad esperada de la imagen jpeg resultante.
+    """
+    _, image_name_and_ext = os.path.split(tif_image_path)
+    image_name, _ = os.path.splitext(image_name_and_ext)
+
+    image = cv2.imread(tif_image_path)
+    new_file = os.path.join(dst_dir, image_name + ".jpg")
+    cv2.imwrite(new_file, image, [cv2.IMWRITE_JPEG_QUALITY, jpeg_quality])
