af220444b31e32ffd8d4233c5fa41824f335e681
Juanjo Alegría <jotaj.8a@gmail.com>
2018-11-07 04:14:59 -0300

    Función para obtener coordenadas mínimas en el eje x que debe tener un parche para ser considerado biopsia y no control

--- src/utils/excel.py
+++ src/utils/excel.py
@@ -17,7 +17,9 @@ ANNOTATION_ID_COLUMN = "Id anotación"
 ANNOTATION_TYPE_COLUMN = "Tipo anotación"
 ANNOTATION_OWNER_COLUMN = "Autor anotación"
 ANNOTATION_LABEL_COLUMN = "Detalles anotación"
+MIN_X_COORD_COLUMN = "Coordenada X inicial (pixeles)"
 SLIDES_NAME_PATTERN = r"229-UCH-(\d*)-IDA"
+
 
 RESECTION_CODE = "Resección"
 ENDOSCOPY_CODE = "Endoscopia"
@@ -139,3 +141,30 @@ def get_valid_annotations_labels(excel_file, valid_owners):
                 row[ANNOTATION_TYPE_COLUMN] in ["freehand", "circle"]:
             valid_annotations[str(index)] = str(row[ANNOTATION_LABEL_COLUMN])
     return valid_annotations
+
+
+def get_min_x_coords(excel_file):
+    """Para cada slide válida, bbtiene la coordenada mínima en el eje x que
+    debe tener un parche extraído de dicha slide para ser considerado como
+    parte de la biopsia y no como parte del tejido control.
+
+    Args:
+        - excel_file: str. Ruta del archivo excel con las clasificaciones.
+
+    Returns:
+        dict[str->int]. Diccionario que a cada id de slide le asocia la
+        coordenada mínima en el eje x para que un parche sea considerado parte
+        de la biopsia y no parte del tejido control.
+    """
+    crossed_info = pd.read_excel(excel_file, sheetname=CROSSED_INFO_SHEET)
+    not_null = ~crossed_info[FINAL_EVALUATION_COLUMN].isnull()
+    valid_rows = crossed_info[not_null]
+
+    valid_slides_names = valid_rows[SLIDES_NAMES_COLUMN].tolist()
+    valid_slides_ids = map(lambda s: re.match(SLIDES_NAME_PATTERN, s).group(1),
+                           valid_slides_names)
+
+    min_x_coords = valid_rows[MIN_X_COORD_COLUMN].tolist()
+    min_x_coords = map(int, min_x_coords)
+
+    return dict(zip(valid_slides_ids, min_x_coords))
